d60ed32e57b05ecef7b05d8bdefc74e42a22b720
skahanium <skahanium@gmail.com>
2021-07-15 14:57:53 +0800

    main

---
+++ python program/第二章 序列构成的数组/2.4.1具名元组1.py
@@ -0,0 +1,8 @@
+from collections import namedtuple
+
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.69, 139.69))
+print(tokyo)
+print(tokyo.name, tokyo.population, tokyo.country, tokyo.coordinates)
+print(City._fields) # _fields属性是一个包含这个类所有字段名称的元组。
+
