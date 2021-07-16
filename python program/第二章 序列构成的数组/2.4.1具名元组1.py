ce8e971c709c0c829c47ef84371568b44595e790
skahanium <skahanium@gmail.com>
2021-07-16 11:14:10 +0800

    main

--- python program/第二章 序列构成的数组/2.4.1具名元组1.py
+++ python program/第二章 序列构成的数组/2.4.1具名元组1.py
@@ -5,4 +5,3 @@ tokyo = City('Tokyo', 'JP', 36.2268, (35.69, 139.69))
 print(tokyo)
 print(tokyo.name, tokyo.population, tokyo.country, tokyo.coordinates)
 print(City._fields) # _fields属性是一个包含这个类所有字段名称的元组。
-
